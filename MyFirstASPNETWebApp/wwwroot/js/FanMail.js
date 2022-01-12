eb1676704a36bed1e564578ccafa572fe39ceb60
Sierra Griffin <sierra.griffin14@gmail.com>
2022-01-12 11:13:45 -0700

    Initial check-in of module MyFirstASPNETWebApp

---
+++ MyFirstASPNETWebApp/wwwroot/js/FanMail.js
@@ -0,0 +1,15 @@
+﻿//document.getElementById("btnSend").addEventListener("click", function () {
+//    //alert("Hey!");
+
+//    alert("From: " + document.getElementById("txtFrom").value +
+//        ", Subject: " + document.getElementById("txtSubject").value +
+//        ", Message: " + document.getElementById("txtMessage").value);
+//})
+
+$("#btnSend").click(function () {
+    alert("From: " + $("#txtFrom").val() +
+        ", Subject: " + $("#txtSubject").val() +
+        ", Message: " + $("#txtMessage").val());
+
+    $("#picGanderson").fadeToggle();
+})
